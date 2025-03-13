263aa0f3c2df3f5f18ee0f82c9ab647c232bd604
NicoKeinBock <n.kranebitter@students.htl-leonding.ac.at>
2025-03-13 14:35:19 +0100

    Arbeiten am Projekt

---
+++ project/javascript/main.js
@@ -0,0 +1,7 @@
+function openSettings(){
+    document.getElementById("settings-popup").style.display = "block";
+
+}
+document.getElementById("close-settings").addEventListener("click", function(){
+    document.getElementById("settings-popup").style.display = "none";
+});
