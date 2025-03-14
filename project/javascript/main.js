d61a333f0bf9c979762b5d8bdfbe7b8972810bac
NicoKeinBock <n.kranebitter@students.htl-leonding.ac.at>
2025-03-14 11:09:52 +0100

    TEST

--- project/javascript/main.js
+++ project/javascript/main.js
@@ -5,3 +5,4 @@ function openSettings(){
 document.getElementById("close-settings").addEventListener("click", function(){
     document.getElementById("settings-popup").style.display = "none";
 });
+//TEST 
