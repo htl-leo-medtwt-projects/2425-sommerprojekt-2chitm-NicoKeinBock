7290c079fb1f4b8e9ed1d4897d38b0ba470f9622
NicoKeinBock <n.kranebitter@students.htl-leonding.ac.at>
2025-03-21 12:39:32 +0100

    SUE_21_03_2025

--- project/javascript/main.js
+++ project/javascript/main.js
@@ -5,3 +5,13 @@ function openSettings(){
 document.getElementById("close-settings").addEventListener("click", function(){
     document.getElementById("settings-popup").style.display = "none";
 });
+let soundState = 1; 
+
+function switchImage(changer){
+    soundState = soundState * changer;
+    if (soundState === 1) {
+        document.getElementById('soundImage').src = "../images/sound_off.png";
+    } else {
+        document.getElementById('soundImage').src = "../images/sound_on.png";
+    }
+}
