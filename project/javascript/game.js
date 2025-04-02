aeb730c2de7b1718f833c60d0f5ce4acef550a82
NicoKeinBock <n.kranebitter@students.htl-leonding.ac.at>
2025-04-02 20:34:13 +0200

    Small Design Changes

--- project/javascript/game.js
+++ project/javascript/game.js
@@ -13,6 +13,27 @@ let MAP = {
     level3: document.getElementById(''),
 
 }
+function gameLoop() {
+  console.log('Game Loop Running');
+  if (KEY_EVENTS.leftArrow) {
+      movePlayer((-1) * GAME_CONFIG.characterSpeed, 0, -1);
+      animatePlayer();
+  }
+  if (KEY_EVENTS.rightArrow) {
+      movePlayer(GAME_CONFIG.characterSpeed, 0, 1);
+      animatePlayer();
+  }
+  if (KEY_EVENTS.upArrow) {
+      movePlayer(0, (-1) * GAME_CONFIG.characterSpeed, 0);
+      animatePlayer();
+  }
+  if (KEY_EVENTS.downArrow) {
+      movePlayer(0, GAME_CONFIG.characterSpeed, 0);
+      animatePlayer();
+  }
+
+  gameLoopTimeout = setTimeout(gameLoop, 1000 / GAME_CONFIG.gameSpeed);
+}
 function movePlayer(dx, dy, dr) {
     // save original position
     let originalX = parseFloat(PLAYER.box.style.left);
@@ -27,7 +48,6 @@ function movePlayer(dx, dy, dr) {
         PLAYER.spriteDirection = dr;
         PLAYER.box.style.transform = `scaleX(${-dr})`; // Flip the sprite direction
     }
-    GAME_SCREEN.debug_output.innerHTML = `x: ${PLAYER.box.style.left} | y: ${PLAYER.box.style.top} | direction: ${dr} | animation: ${PLAYER.spriteImgNumber} | count: ${PLAYER.coinCount}`;
 }
 function animatePlayer() {
     if (PLAYER.spriteImgNumber < 3) {
